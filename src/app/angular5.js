e631bf6feb7e6db6585a886c81e03aa94951e463
Chetan Sachdev <cksachdev@gmail.com>
2014-12-26 23:00:04 +0530

    Changed to BlogService

--- src/app/angular5.js
+++ src/app/angular5.js
@@ -3,12 +3,35 @@
 
     var app = angular.module('myblog', ['ENV']);
 
-    app.controller('BlogController', ['$http', 'GOOGLE_KEY', function($http, googleKey){
+    app.controller('BlogController', function($scope, BlogService){
         var blog = this;
         blog.posts = [];
-        $http.get('https://www.googleapis.com/blogger/v3/blogs/33279377/posts?key=' + googleKey).success(function(data){
-            blog.posts = data["items"];
-        });
+        BlogService.fetch().then(function(result){
+            console.log(result);
+            blog.posts = result.items;
+        }, function(reason){
+            console.log('ERROR', reason);
+        });;
+
+        console.log($scope.posts);
+    });
+
+    app.factory('BlogService', ['$http', 'GOOGLE_KEY', '$q', function($http, googleKey, $q){
+        var baseUrl = 'https://www.googleapis.com/blogger/v3/blogs/33279377/posts?key=';
+        var key = googleKey;
+
+        var fetch = function (board_id) {
+            var deferred = $q.defer();
+            var url = baseUrl + key;
+
+            $http.get(url).success(deferred.resolve).error(deferred.reject)
+
+            return deferred.promise;
+        };
+
+        return {
+            fetch: fetch
+        }
     }]);
 
 })();
